61135a2e1af2dd8e435e0d4a78410810a48a0329
cutty12 <cuttyswiss@protonmail.com>
2020-08-15 18:46:57 +0200

    correct typo

--- healthpassport/settings.py
+++ healthpassport/settings.py
@@ -22,7 +22,7 @@ import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
-DATABASES['default'] = dj_database_url.config(default='postgres://...'}
+DATABASES['default'] = dj_database_url.config(default='postgres://...')
 
 DATABASES['default'] = dj_database_url.parse('postgres://...', conn_max_age=600)
 
