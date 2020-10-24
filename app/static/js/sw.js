14f7086b65c8b599f4cf11cb068a0de61e3b55fd
cutty12 <cuttyswiss@protonmail.com>
2020-10-24 17:32:52 +0200

    remove manifest versions

--- app/static/js/sw.js
+++ app/static/js/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'Healthpassport-v1';
 
 var urlsToCache = [
     '/',
--- healthpassport/settings.py
+++ healthpassport/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'app/static/js', 'sw.js')
+# PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'app/static/js', 'sw.js')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
